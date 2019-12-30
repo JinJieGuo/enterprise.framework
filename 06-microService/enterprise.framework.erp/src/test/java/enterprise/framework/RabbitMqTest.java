ca118c4daf2b9b5e8b3006c14a39827ddfd24db4
gl <gl_0213@163.com>
2019-12-31 00:04:35 +0800

    添加单元测试消费者

--- 06-microService/enterprise.framework.erp/src/test/java/enterprise/framework/RabbitMqTest.java
+++ 06-microService/enterprise.framework.erp/src/test/java/enterprise/framework/RabbitMqTest.java
@@ -19,11 +19,17 @@
 
 package enterprise.framework;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DeliverCallback;
 import enterprise.framework.core.rabbitmq.IMQManager;
 import enterprise.framework.core.rabbitmq.MQManager;
+import enterprise.framework.core.rabbitmq.RabbitMqInfo;
 import enterprise.framework.erp.ErpApplication;
+import enterprise.framework.utility.transform.MapHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -34,6 +40,9 @@ import java.util.concurrent.TimeoutException;
 @SpringBootTest(classes = ErpApplication.class)
 public class RabbitMqTest {
 
+    @Autowired(required = false)
+    public enterprise.framework.core.rabbitmq.RabbitMq rabbitMq;
+
     @Test
     public void ConnectionRabbitTest() throws IOException, TimeoutException {
         IMQManager imqManager = new MQManager();
@@ -41,7 +50,29 @@ public class RabbitMqTest {
     }
 
     @Test
-    public void receivedMsg(){
+    public void receivedMsg() throws IOException, TimeoutException, InterruptedException {
+        IMQManager imqManager = new MQManager();
+        enterprise.framework.core.rabbitmq.RabbitMqInfo rabbitMqInfo = MapHandler.mapToObject(rabbitMq.getRabbitMqInfo().get(0), RabbitMqInfo.class);
+        Connection rabbitMqConnection = imqManager.instance().createConnection(rabbitMq.getRabbitMqInfo().get(0));
+        Channel channel = rabbitMqConnection.createChannel();
+//        channel.basicQos(0, 1, false);
+//        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+//            String message = new String(delivery.getBody(), "UTF-8");
+//            System.out.println(" [x] Received '" + message + "'");
+//        };
+//        channel.basicConsume("msg-server-alpha-01", true, deliverCallback, consumerTag -> {
+//        });
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        };
+        channel.basicConsume("msg-server-alpha-01", false, deliverCallback, consumerTag -> {
+        });
+        do {
+            Thread.sleep(100000000L);
+        } while (true);
 
     }
 }
